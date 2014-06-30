f332b88b47f605450dfc10cd46d87f32e5d0206a
philzep <phil.zepeda@msnbc.com>
2014-06-30 17:00:36 -0400

    Changes

--- main2.js
+++ main2.js
@@ -1,3 +1,30 @@
 $(document).ready(function() {
+	var open = false;
+	$('.vid-container').on('click', function(e){
+		if (open) {
+			open = false;
+			$('#player-container').animate({height:170})
+		} else {
+			open = true;
+			$('#player-container').animate({height:337})
+		}
+	});
 
+	$('#j-rail-tease').on('mouseover', function(e){
+		if (open) {
+			open = false;
+			$('#player-container').animate({height:170})
+		} else {
+			open = true;
+			$('#player-container').animate({height:337})
+		}
+	}).on('mouseout', function(e){
+		if (open) {
+			open = false;
+			$('#player-container').animate({height:170})
+		} else {
+			open = true;
+			$('#player-container').animate({height:337})
+		}
+	});
 });
