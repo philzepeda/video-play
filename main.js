5c1f75a0ef25d48befee467c08f582bbb94eb893
Shezad Morani <smorani@gmail.com>
2014-07-01 16:25:21 -0400

    presentation

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
 	$('#player').on('mouseover', function() {
-		$('#player-container').addClass('player-bg');
-	}).on('mouseout', function() {
-		$('#player-container').removeClass('player-bg');
+		$('.navbar').addClass('max');
+		$('#player-container').addClass('active');
+		$('.video-nav-container').addClass('active');
+		$('#player').addClass('medium');
+	})
+	$('.exit').on('click', function() {
+		$('.navbar').removeClass('max');
+		$('#player-container').removeClass('active');
+		$('.video-nav-container').removeClass('active');
+		$('#player').removeClass('medium');
 	})
 });
